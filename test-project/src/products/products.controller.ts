f6966e553539ba0fa32833d4c3cb420decc41301
Bin Chen <binchen.devops@gmail.com>
2020-08-26 10:41:43 -0400

    linted code in products directory

--- test-project/src/products/products.controller.ts
+++ test-project/src/products/products.controller.ts
@@ -4,45 +4,45 @@ import { ProductsService } from "./products.service";
 @Controller('products')
 export class ProductsController {
 
-	constructor(private readonly productsService: ProductsService){}
+    constructor(private readonly productsService: ProductsService){}
 
-	// content header generaeted automatically.
-	@Post()
-	addProduct(
-		@Body('title') prodTitle: string,
-		@Body('description') prodDesc: string,	
-	    @Body('price') prodPrice: number	
-	): any {
-		const id = this.productsService.insertProduct(
-			prodTitle, prodDesc, prodPrice
-		);
-		return {id:id};
-	}
+    // content header generaeted automatically.
+    @Post()
+    addProduct(
+        @Body('title') prodTitle: string,
+        @Body('description') prodDesc: string,    
+        @Body('price') prodPrice: number    
+    ): any {
+        const id = this.productsService.insertProduct(
+            prodTitle, prodDesc, prodPrice
+        );
+        return {id:id};
+    }
 
-	@Get()
-	getAllProducts(){
-		return this.productsService.getProducts();
-	}
+    @Get()
+    getAllProducts(){
+        return this.productsService.getProducts();
+    }
 
-	@Get(":id")
-	getProduct(@Param('id') id: string) {
-		return this.productsService.getProduct(id);
-	}
-	
-	@Delete(":id")
-	deleteProduct(@Param('id') id: string) {
-		return this.productsService.deleteProduct(id);
-	}
-	
-	@Patch(":id")
-	updateProduct(
-		@Param('id') id: string,
-		@Body('title') title: string,
+    @Get(":id")
+    getProduct(@Param('id') id: string) {
+        return this.productsService.getProduct(id);
+    }
+    
+    @Delete(":id")
+    deleteProduct(@Param('id') id: string) {
+        return this.productsService.deleteProduct(id);
+    }
+    
+    @Patch(":id")
+    updateProduct(
+        @Param('id') id: string,
+        @Body('title') title: string,
         @Body('description') description: string,
-        @Body('price') price: number		
-	) {
-		return this.productsService.updateProduct(id, title, description, price);
-	}
+        @Body('price') price: number        
+    ) {
+        return this.productsService.updateProduct(id, title, description, price);
+    }
 }
 
 
